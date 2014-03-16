a35ce444fbfd25c2dcd2d0bf06adea47d0d5e1fa
fzhan <fzhan@fzhan-X202E.(none)>
2014-03-15 18:18:38 -0700

    add comment

--- src/moeillustra.js
+++ src/moeillustra.js
@@ -1,9 +1,13 @@
+// moegirl illustration
+
 var MOEILLUSTRA = MOEILLUSTRA || new Object();
 MOEILLUSTRA.config = {
 	files: ['data/moegirls.json'],
 	canvas: undefined,
 	count: 0,
+	current_illustra: 0,
 };
+// initialize by loading json files
 MOEILLUSTRA.init = function () {
 	function loadJson(file) {
 		$.getJSON(file, function(data, textStatus, jqXHR) {
@@ -16,22 +20,27 @@ MOEILLUSTRA.init = function () {
 		.always(function(data, textStatus, jqXHR) {
 		});
 	}
+	// load json files
 	for (var i in MOEILLUSTRA.config.files) {
 		loadJson(MOEILLUSTRA.config.files[i]);
 	}
 };
+// init when page loaded
 $(function() {
 	MOEILLUSTRA.init();
 });
+// main function
 MOEILLUSTRA.run = function (canvas) {
 	if (undefined === canvas || $("#" + canvas).length == 0) {
 		console.error("undefined canvas", canvas);
 		return;
 	}
+	// save canvas id
 	MOEILLUSTRA.config.canvas = canvas;
 	MOEILLUSTRA.showFrame();
 	MOEILLUSTRA.waitLoad();
 };
+// wait for loading json
 MOEILLUSTRA.waitLoad = function () {
 	if (0 == MOEILLUSTRA.illustra.length) {
 		console.log("loading");
@@ -40,60 +49,71 @@ MOEILLUSTRA.waitLoad = function () {
 	}
 	MOEILLUSTRA.showRandom();
 };
+// illustrations for moegirls
 MOEILLUSTRA.illustra = new Array();
+// load illustrations from json
 MOEILLUSTRA.loadIllustra = function (data) {
-	for (var i in data) {
-		/*if (! (i in MOEILLUSTRA.illustra)) {
-			MOEILLUSTRA.illustra[i] = data[i];
-			++ MOEILLUSTRA.config.count;
-		} else {
-			console.warn("data conflict", i);
-		}*/
-		MOEILLUSTRA.illustra.push(data[i]);
-	}
+	MOEILLUSTRA.illustra = MOEILLUSTRA.illustra.concat(data);
 	console.log("loaded illustra ", MOEILLUSTRA.illustra.length);
 }
+// show the framework
 MOEILLUSTRA.showFrame = function () {
 	var canvas = $("#" + MOEILLUSTRA.config.canvas);
+	// clear canvas
 	canvas.empty();
+	// create image photo
 	canvas.append($('<a href="#" class="id-pagelink pagelink" target="_blank"><img border="0" src="resources/nophoto.jpg" alt="photo" width="20%" class="id-photo photo" /></a>'));
 	$("#" + MOEILLUSTRA.config.canvas + " .id-photo").hide();
+	// create random button
 	canvas.append($('<button type="button" class="id-randombutton button randombutton">随机</button>'));
 	$("#" + MOEILLUSTRA.config.canvas + " .id-randombutton").click(MOEILLUSTRA.showRandom);
+	// create an empty table for information
 	canvas.append($('<table class="id-table" border="0"></table>'));
 };
+// show a random illustra
+MOEILLUSTRA.showRandom = function () {
+	// random
+	var r = Math.floor( Math.random() * MOEILLUSTRA.illustra.length );
+	MOEILLUSTRA.config.current_illustra = r;
+	MOEILLUSTRA.showIllustra(MOEILLUSTRA.illustra[MOEILLUSTRA.config.current_illustra]);
+}
+// show an illustra
 MOEILLUSTRA.showIllustra = function (illustra) {
 	var table = $("#" + MOEILLUSTRA.config.canvas + " .id-table");
+	// clear the table
 	table.empty();
 	table.append($('<tr><th class="id-th0"></th><th class="id-th1"></th></tr>'));
+	// clear link and photo
 	$("#" + MOEILLUSTRA.config.canvas + " .id-pagelink").attr("href", "#");
 	$("#" + MOEILLUSTRA.config.canvas + " .id-photo").hide();
 	$("#" + MOEILLUSTRA.config.canvas + " .id-photo").attr("src", "resources/nophoto.jpg");
+	// for each attribute
 	for (var i in illustra) {
+		// append photo
 		if ("photo" == i) {
 			$("#" + MOEILLUSTRA.config.canvas + " .id-photo").attr("src", illustra[i]);
 		}
+		// append link
 		else if ("link" == i) {
 			$("#" + MOEILLUSTRA.config.canvas + " .id-pagelink").attr("href", illustra["link"]);
-		} else {
+		} else { // append an attribute
 			var td = illustra[i];
+			// stringify a json
 			if (typeof illustra[i] == "array" || typeof illustra[i] == "object") {
 				td = JSON.stringify(illustra[i]);
 			}
+			// if too long
 			if (td.length > 30) {
 				//td = td.substring(0, 30) + "...";
 			}
 			table.append($('<tr><td>' + i + '</td><td>' + td + '</td></tr>'));
 		}
 	}
+	// fade in
 	$("#" + MOEILLUSTRA.config.canvas + " .id-photo").hide().css({visibility: "inherit"}).fadeIn("slow");
 	table.hide().css({visibility: "inherit"}).fadeIn("slow");
 };
-MOEILLUSTRA.showRandom = function () {
-	var r = Math.floor( Math.random() * MOEILLUSTRA.illustra.length );
-	MOEILLUSTRA.config.current_illustra = r;
-	MOEILLUSTRA.showIllustra(MOEILLUSTRA.illustra[MOEILLUSTRA.config.current_illustra]);
-}
+
 
 
 
--- src/moequest.js
+++ src/moequest.js
@@ -1,11 +1,16 @@
+// moegirl quest
+
 var MOEQUEST = MOEQUEST || new Object();
 MOEQUEST.config = {
 	files: ['data/moegirls.json'],
 	canvas: undefined,
 	ans_canvas: undefined,
-	current_quest: 0,
+	// 4 options for a question
+	option_num: 4,
+	quest: new Object(),
 	results: new Array(),
 };
+// initialize by loading json files
 MOEQUEST.init = function () {
 	function loadJson(file, func) {
 		$.getJSON(file, function(data, textStatus, jqXHR) {
@@ -20,27 +25,31 @@ MOEQUEST.init = function () {
 		.always(function(data, textStatus, jqXHR) {
 		});
 	}
+	// load json files
 	for (var i in MOEQUEST.config.files) {
 		loadJson(MOEQUEST.config.files[i], function (data) {
-			for (var i in data) {
-				MOEQUEST.moegirls.push(data[i]);
-			}
+			// add to moegirl list
+			MOEQUEST.moegirls = MOEQUEST.moegirls.concat(data);
 		});
 	}
 };
+// init when page loaded
 $(function() {
 	MOEQUEST.init();
 });
+// main function
 MOEQUEST.run = function (canvas, ans) {
 	if (undefined === canvas || $("#" + canvas).length == 0) {
 		console.error("undefined canvas", canvas);
 		return;
 	}
+	// save canvas id
 	MOEQUEST.config.canvas = canvas;
 	MOEQUEST.config.ans_canvas = ans;
 	MOEQUEST.showFrame();
 	MOEQUEST.waitLoad();
 };
+// wait for loading files
 MOEQUEST.waitLoad = function () {
 	if ((typeof MOEQUEST.moegirls != "array" && typeof MOEQUEST.moegirls != "object") || MOEQUEST.moegirls.length == 0) {
 		console.log("loading");
@@ -49,30 +58,45 @@ MOEQUEST.waitLoad = function () {
 	}
 	MOEQUEST.showQuest();
 };
-MOEQUEST.moegirls = new Array();
-MOEQUEST.question;
+// show the framework
 MOEQUEST.showFrame = function () {
 	var canvas = $("#" + MOEQUEST.config.canvas);
+	// clear the canvas
 	canvas.empty();
-	canvas.append($('<div class="id-question question"><span></span></div>'));
+	// append the question
+	canvas.append($('<div class="id-question question"><span></span><img src="" alt="question" class="" /></div>'));
+	$("#" + MOEQUEST.config.canvas + " .id-question img").error(function () {
+		$(this).attr("src", "resources/nophoto.jpg");
+	});
+	// append the last result
 	canvas.append($('<img border="0" src="resources/correct.ico" alt="result" width="10%" class="id-lastresult lastresult" />'));
+	// append submit button
 	canvas.append($('<button type="button" class="id-submitbutton button submitbutton">Answer</button>'));
 	$("#" + MOEQUEST.config.canvas + " .id-submitbutton").click(MOEQUEST.submitQuest);
-	canvas.append($('<button type="button" class="id-clearbutton button clearbutton">Clear</button>'));
-	$("#" + MOEQUEST.config.canvas + " .id-clearbutton").click(MOEQUEST.clearChecked);
+	// append clear button
+	/*canvas.append($('<button type="button" class="id-clearbutton button clearbutton">Clear</button>'));
+	$("#" + MOEQUEST.config.canvas + " .id-clearbutton").click(MOEQUEST.clearChecked);*/
 
+	// append options
 	canvas.append($('<div class="id-optionsection optionsection" />'));
 	html = $("#" + MOEQUEST.config.canvas + " .id-optionsection");
-	for (var i = 0; i < 4; ++ i) {
+	// for each option
+	for (var i = 0; i < MOEQUEST.config.option_num; ++ i) {
 		html.append($('<div class="id-option' + i + ' option" />'));
 		var html2 = $("#" + MOEQUEST.config.canvas + " .id-option" + i);
-		html2.append('<input type="checkbox" name="check' + i + '" value="' + i + '" class="id-check' + i + ' check" />').append('<span></span>');
+		// append a checkbox
+		html2.append('<input type="checkbox" name="check' + i + '" value="' + i + '" class="id-check' + i + ' check" />').append('<span></span><img src="" alt="question" class="" />');
+		$("#" + MOEQUEST.config.canvas + " .id-option" + i + " img").error(function () {
+			$(this).attr("src", "resources/nophoto.jpg");
+		});
+		// append click event for span, clicking the words has the same effect with checkbox
 		$("#" + MOEQUEST.config.canvas + " .id-option" + i + " span").click(function () {
 			var check = $(this).prev();
 			check.trigger('click'); 
 		});
 	}
 
+	// for answer canvas
 	canvas = $("#" + MOEQUEST.config.ans_canvas);
 	canvas.empty();
 	canvas.append($('<div class="id-question question"><span></span></div>'));
@@ -84,26 +108,48 @@ MOEQUEST.showFrame = function () {
 		html2.append('<span></span>');
 	}
 };
+// show the quest
 MOEQUEST.showQuest = function () {
+	// create a quest
 	quest = MOEQUEST.createQuest();
-	MOEQUEST.question = quest;
-	$("#" + MOEQUEST.config.canvas + " .id-question span").html(quest.quest);
+	// show question
+	$("#" + MOEQUEST.config.canvas + " .id-question span").html(quest.question);
+	// show question image
+	if (undefined !== quest.question_img) {
+		$("#" + MOEQUEST.config.canvas + " .id-question img").attr("src", quest.question_img);
+		$("#" + MOEQUEST.config.canvas + " .id-question img").addClass("show");
+	} else { // hide image
+		$("#" + MOEQUEST.config.canvas + " .id-question img").attr("src", "");
+		$("#" + MOEQUEST.config.canvas + " .id-question img").removeClass("show");
+	}
+	// for each option
 	for (var i = 0; i < quest.options.length; ++ i) {
 		var option = $("#" + MOEQUEST.config.canvas + " .id-option" + i + " span");
 		if (option.length == 0) {
 			console.warn("option canvas not enough", i, quest.options);
 			break;
 		}
+		// show option
 		option.html(quest.options[i]);
+		// show option image
+		if (undefined !== quest.option_img && undefined !== quest.option_img[i]) {
+			$("#" + MOEQUEST.config.canvas + " .id-option" + i + " img").attr("src",quest.option_img[i]);
+			$("#" + MOEQUEST.config.canvas + " .id-option" + i + " img").addClass("show");
+		} else { // hide image
+			$("#" + MOEQUEST.config.canvas + " .id-option" + i + " img").attr("src","");
+			$("#" + MOEQUEST.config.canvas + " .id-option" + i + " img").removeClass("show");
+		}
 	}
+	// fade in
 	$("#" + MOEQUEST.config.canvas + " .id-question").hide().css({visibility: "inherit"}).fadeIn("slow");
 	$("#" + MOEQUEST.config.canvas + " .option").hide().css({visibility: "inherit"}).fadeIn("slow");
 };
+// show the answer
 MOEQUEST.showAnswer = function (quest) {
 	if (undefined === MOEQUEST.config.ans_canvas || $("#" + MOEQUEST.config.ans_canvas).length == 0) {
 		return;
 	}
-	$("#" + MOEQUEST.config.ans_canvas + " .id-question span").text(quest.quest);
+	$("#" + MOEQUEST.config.ans_canvas + " .id-question span").text(quest.question);
 	for (var i = 0; i < quest.options.length; ++ i) {
 		var option = $("#" + MOEQUEST.config.ans_canvas + " .id-option" + i + " span");
 		if (option.length == 0) {
@@ -134,18 +180,23 @@ MOEQUEST.showAnswer = function (quest) {
 		$("#" + MOEQUEST.config.ans_canvas).hide().css({visibility: "inherit"}).fadeIn("slow");
 	});
 };
+// submit a quest
 MOEQUEST.submitQuest = function () {
 	MOEQUEST.checkAnswer();
+	// if the max number has reached
 	if (false) {
-		console.debug("game over");
+		console.log("game over");
 		return;
 	}
 	MOEQUEST.clearChecked();
+	// show the next one
 	MOEQUEST.showQuest();
 };
+// check if the answer is correct
 MOEQUEST.checkAnswer = function () {
-	var quest = MOEQUEST.question;
+	var quest = MOEQUEST.config.quest;
 	var ans = new Array();
+	// get user's answer
 	$("#" + MOEQUEST.config.canvas + " .id-optionsection .check").each(function (index, value) {
 		var check = $(value)[0];
 		var val = parseInt($(value).attr('value'));
@@ -158,6 +209,7 @@ MOEQUEST.checkAnswer = function () {
 		}
 	});
 	var wrong_flag = false;
+	// answer number is wrong
 	if (ans.length != quest.correct.length) {
 		wrong_flag = true;
 	} else {
@@ -170,37 +222,58 @@ MOEQUEST.checkAnswer = function () {
 			}
 		}
 	}
+	// append to result list
 	MOEQUEST.config.results.push(! wrong_flag);
+	// show last result image
 	$("#" + MOEQUEST.config.canvas + " .id-lastresult").attr("src",'resources/' + (wrong_flag ? "incorrect" : "correct") + '.ico').hide().css({visibility: "inherit"}).fadeIn("slow");
 	MOEQUEST.showAnswer(quest);
 };
+// clear checked checkboxes
 MOEQUEST.clearChecked = function () {
 	$("#" + MOEQUEST.config.canvas + " .id-optionsection .check").each(function (index, value) {
 		$(value).attr('checked', false); 
 	});
 };
+// create a quest
 MOEQUEST.createQuest = function () {
+	MOEQUEST.config.quest = MOEQUEST.createMoegirlQuest();
+	window["peek"] = MOEQUEST.config.quest.correct;
+	return MOEQUEST.config.quest;
+}
+// moegirl list
+MOEQUEST.moegirls = new Array();
+// create a quest based on moegirl list
+MOEQUEST.createMoegirlQuest = function () {
 	var quest = {
-		quest: "",
+		question: "",
+		question_img: undefined,
 		options: [],
+		option_img: [],
 		correct: []
 	};
-	var OPTION_NUM = 4;
+	// choose some moegirls as options
 	var choices = new Array();
-	if (MOEQUEST.moegirls.length < OPTION_NUM) {
+	// if too few moegirls
+	if (MOEQUEST.moegirls.length < MOEQUEST.config.option_num) {
+		// add all
 		for (var i in MOEQUEST.moegirls) {
 			choices.push(i);
 		}
 	} else {
-		while (choices.length < OPTION_NUM) {
+		// choose by random
+		while (choices.length < MOEQUEST.config.option_num) {
 			var ran = Math.floor( (Math.random() * MOEQUEST.moegirls.length) );
+			// check if duplicate
 			if (choices.indexOf(ran) <= -1) {
 				choices.push(ran);
 			}
 		}
 	}
-	var QUEST_ATTR = ["photo"];
+	// attributes for question
+	var QUEST_ATTR = ["photo", '发色', '瞳色', '声优', '身高'];
+	// choose an attribute by random
 	var ran = Math.floor( (Math.random() * QUEST_ATTR.length) );
+	// a list of moegirls with valid attribute
 	var good_choices = new Array();
 	for (var i in choices) {
 		if (undefined !== MOEQUEST.moegirls[choices[i]][QUEST_ATTR[ran]]) {
@@ -208,10 +281,12 @@ MOEQUEST.createQuest = function () {
 			break;
 		}
 	}
+	// no one has selected attribute
 	if (0 == good_choices.length) {
 		console.warn("question attr fails to find a good choice", QUEST_ATTR[ran]);
 		return;
 	}
+	// choose one as answer by random
 	var answer = Math.floor( (Math.random() * good_choices.length) );
 	if (choices.indexOf(good_choices[answer]) <= -1) {
 		console.warn("invalid answer", MOEQUEST.moegirls[good_choices[answer]].name);
@@ -220,20 +295,39 @@ MOEQUEST.createQuest = function () {
 	// shuffle choices
 	for (var j, x, i = choices.length; i; j = Math.floor(Math.random() * i), x = choices[--i], choices[i] = choices[j], choices[j] = x)
 	{}
+	// find correct ones
     for (var i in choices) {
 		if (MOEQUEST.moegirls[good_choices[answer]][QUEST_ATTR[ran]] == MOEQUEST.moegirls[choices[i]][QUEST_ATTR[ran]]) {
 			quest.correct.push(i);
 		}
 	}
+	// assign names to options
 	for (var i in choices) {
 		quest.options.push(MOEQUEST.moegirls[choices[i]].name);
+		if ("photo" != QUEST_ATTR[ran]) {
+			quest.option_img.push(MOEQUEST.moegirls[choices[i]].photo);
+		}
 	}
+	// assign question sentence
 	switch (QUEST_ATTR[ran]) {
 	case "photo":
-		quest.quest = "该图片是哪位萌娘？" + '<img src="' + MOEQUEST.moegirls[good_choices[answer]].photo + '" alt="guess" style="vertical-align:middle;" />';
+		quest.question = "该图片是哪位萌娘？";
+		quest.question_img = MOEQUEST.moegirls[good_choices[answer]].photo;
+		break;
+	case '发色':
+		quest.question = "哪位萌娘的发色是<strong>" + MOEQUEST.moegirls[good_choices[answer]]['发色'] + "</strong>？";
+		break;
+	case '瞳色':
+		quest.question = "哪位萌娘的瞳色是<strong>" + MOEQUEST.moegirls[good_choices[answer]]['瞳色'] + "</strong>？";
+		break;
+	case '声优':
+		quest.question = "哪位萌娘的声优是<strong>" + MOEQUEST.moegirls[good_choices[answer]]['声优'] + "</strong>？";
+		break;
+	case '身高':
+		quest.question = "哪位萌娘的身高是<strong>" + MOEQUEST.moegirls[good_choices[answer]]['身高'] + "</strong>？";
 		break;
 	default:
-		console.warn("invalid question attr", QUEST_ATTR[ran]);
+		quest.question = "答案是哪位萌娘？";
 		return;
 		break;
 	}
